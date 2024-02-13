8a816fd309ccadd40c13bd092b4e966fc281d608
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-13 15:21:23 +0500

    mmmmmmmmmmmmmmmmm

--- index.js
+++ index.js
@@ -165,3 +165,4 @@ console.log("problem 6 ni ohrida tushinmochilik bor");
 console.log("problem 6 ni ohrida tushinmochilik bor");
 console.log("problem 6 ni ohrida tushinmochilik bor");
 console.log("problem 6 ni ohrida tushinmochilik bor");
+console.log("problem 6 ni ohrida tushinmochilik bor");
