4b1336c6ef51e66a673079057eed1589d0d448df
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-13 13:33:41 +0500

    animals

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Assalomu alekum");
+//problem1
+// var wild_animals = function () {
+//   var animals = {
+//     bear: {
+//       color: "brown",
+//       weight: 300,
+//     },
+//     lion: {
+//       color: "yellow",
+//       weight: 150,
+//     },
+//   };
+//   //create
+//   animals["wolf"] = {
+//     color: "black",
+//     weight: 100,
+//   };
+
+//   //update
+//   animals.lion.weight = 200;
+
+//   //delete
+//   delete animals.bear;
+
+//   console.log(animals);
+// };
+
+// wild_animals();
+//================================================================================
