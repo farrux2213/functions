4f50bc5867788512bbbea11e314448e5f9d767dd
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-14 20:32:47 +0500

    sdbuvds

--- index.js
+++ index.js
@@ -179,6 +179,8 @@ console.log(Math.sqrt(9137019471));
 console.log("6 erron one");
 console.log("6 erron one");
 console.log("6 erron one");
+console.log("6 erron one");
+console.log("6 erron one");
 
 console.log(Math.sqrt(9137019471));
 console.log(Math.sqrt(9137019471));
