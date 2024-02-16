e12af41d5f17453b51c45330d06cf12764ff31fc
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-16 21:44:53 +0500

    sdvosb

--- index.js
+++ index.js
@@ -141,5 +141,7 @@ lost.key3 = "value3";
 
 console.log(Object.keys(lost));
 console.log(Object.values(lost));
+console.log(Object.values(lost));
+console.log(Object.values(lost));
 console.log(Object.entries(lost));
 console.log(Object.entries(lost));
